c2cbf32c697f61a1bdccf9e79a977793bca371be
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-12 23:42:58 +0300

    Refactored and moved stuff to Request class

--- lib/Context.php
+++ lib/Context.php
@@ -1,11 +1,10 @@
 <?php
 
-// Refactor this to work with a URL class
+// TODO Refactor this to work with a URL class
 class Context {
 
 	private $uri = '';
-	private $path = '';
-	private $method = 'UNKNOWN';
+	private $payload = '';
 
 	private $action = null;
 	private $subject = null;
@@ -13,80 +12,47 @@ class Context {
 
 	private $content = null;
 
-	// TODO refactor to Config or DB or whatever
-	private $action_list = [
-		'User', 'Tarif',
-		];
-
 	public function __construct() {
-		$this->setRequestMethod();
-		$this->setUri();
-		$this->setPath();
-		$this->setRoute();
-	}
-
-	public function getRequestMethod() {
-		return $this->method;
-	}
-
-	public function setRequestMethod() {
-		if (isset($_SERVER['REQUEST_METHOD'])) {
-			$this->method = $_SERVER['REQUEST_METHOD'];
-		}
 	}
 
 	public function getUri() {
 		return $this->uri;
 	}
 
-	public function setUri() {
-		if (isset($_SERVER['REQUEST_URI'])) {
-			$this->uri = $_SERVER['REQUEST_URI'];
-		}
+	public function setUri($uri) {
+		$this->uri = $uri;
 	}
 
-	public function getPath() {
-		return $this->path;
+	public function getPayload() {
+		return $this->payload;
 	}
 
-	public function setPath() {
-		// Strip out 'api' part
-		$uri = str_replace('api', '', $this->uri);
-		$this->path = trim($uri, '/');
-
-		// Get action that is coming from Query String
-		$this->argv = explode('/', $this->path);
-		$this->action = array_pop($this->argv);
+	public function setPayload($payload) {
+		$this->payload = $payload;
 	}
 
 	public function getAction() {
 		return $this->action;
 	}
 
-	public function getSubject() {
-		return $this->subject;
+	public function setAction($action) {
+		$this->action = $action;
 	}
 
 	public function getArgs() {
 		return $this->argv;
 	}
 
+	public function setArgs(array $argv) {
+		$this->argv = $argv;
+	}
+
+	// Returns individual argument
 	public function getArg(int $arg) {
 		if (isset($this->argv[$arg])) {
 			return $this->argv[$arg];
 		} else {
 			return false;
-		}
-	}
-
-	// Sets argument vector to run an application
-	// TODO refactor
-	public function setRoute() {
-		// Collect all the arguments and lightly sanitize them
-		foreach ($_POST as $k => $v) {
-			if (!empty($v)) {
-				$this->argv[$k] = htmlspecialchars($v);
-			}
 		}
 	}
 
